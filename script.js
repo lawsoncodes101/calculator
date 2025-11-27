b58fb3373661f090d2dc8e28209a9919dd1878f9
lawson101 <lawsoncodes101@gmail.com>
2025-11-27 02:06:21 +0100

    Implemented basic arithemetic functions

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const firstNum = 0;
-const operator = "";
-const lastNum = 0;
+const operate = function (firstNum, op, lastNum) {
+    switch (op) {
+        case "+":
+            const add = function (a, b) {
+                return a + b;
+            };
+            return add(firstNum, lastNum);
+        case "-":
+            const subtract = function (a, b) {
+                return a - b;
+            };
+            return subtract(firstNum, lastNum);
+        case "*" || "x":
+            const multiply = function (a, b) {
+                return a * b;
+            };
+            return multiply(firstNum, lastNum);
+        case "/":
+            const divide = function (a, b) {
+                return a / b;
+            };
+            return divide(firstNum, lastNum);
+    }
+};
 
-function Calculator () {
-    this.methods = {};
-
-    
-}
+console.log(operate(2, "+", 3));

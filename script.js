d20729f337a86e853fbc2c521bae33d29a0c810b
lawson101 <lawsoncodes101@gmail.com>
2025-11-27 15:20:51 +0100

    implemented html structure...

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const operate = function (firstNum, op, lastNum) {
-    switch (op) {
-        case "+":
-            const add = function (a, b) {
-                return a + b;
-            };
-            return add(firstNum, lastNum);
-        case "-":
-            const subtract = function (a, b) {
-                return a - b;
-            };
-            return subtract(firstNum, lastNum);
-        case "*" || "x":
-            const multiply = function (a, b) {
-                return a * b;
-            };
-            return multiply(firstNum, lastNum);
-        case "/":
-            const divide = function (a, b) {
-                return a / b;
-            };
-            return divide(firstNum, lastNum);
-    }
+   switch (op) {
+      case "+":
+         const add = function (a, b) {
+            return a + b;
+         };
+         return add(firstNum, lastNum);
+      case "-":
+         const subtract = function (a, b) {
+            return a - b;
+         };
+         return subtract(firstNum, lastNum);
+      case "*":
+      case "x":
+         const multiply = function (a, b) {
+            return a * b;
+         };
+         return multiply(firstNum, lastNum);
+      case "/":
+         const divide = function (a, b) {
+            return a / b;
+         };
+         return divide(firstNum, lastNum);
+   }
 };
-
-console.log(operate(2, "+", 3));
+console.log(operate(2, "x", 3));
